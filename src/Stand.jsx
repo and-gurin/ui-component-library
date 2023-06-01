9076c54eecc4f221a2a8a3ebf2819063ef1fcd4a
and-gurin <and.gurin@gmail.com>
2023-06-01 12:21:58 +0200

    feat: add additional functionality to button component

--- src/Stand.jsx
+++ src/Stand.jsx
@@ -1,20 +1,139 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Button from 'buttons/Button';
-import iconMessageLight from 'assets/svg/icon-message-light.svg'
+import {Slider} from "@mui/material";
 
+const xTypes = [
+    {id: 1, value: 'primary'},
+    {id: 2, value: 'secondary'},
+    {id: 3, value: 'outline'},
+    {id: 4, value: 'transparent'}
+]
+
+const buttonHeights = [
+    {id: 1, value: '48px'},
+    {id: 2, value: '32px'},
+]
+
+const iconWidths = [
+    {id: 1, value: '16px'},
+    {id: 2, value: '24px'},
+]
+
+const iconPlaces = [
+    {id: 1, value: 'iconLeft'},
+    {id: 2, value: 'iconRight'},
+    {id: 3, value: 'iconSide'},
+]
 
 const Stand = () => {
+    const [xType, setXType] = useState('')
+    const [disabled, setDisabled] = useState(false)
+    const [buttonHeight, setButtonHeight] = useState('')
+    const [iconPlace, setIconPlace] = useState('')
+    const [buttonWidth, setButtonWidth] = useState(400)
+    const [titlePlace, setTitlePlace] = useState(100)
+    const [iconMarginLeft, setIconMarginLeft] = useState(100)
+    const [iconMarginRight, setIconMarginRight] = useState(100)
+    const [isIcon, setIcon] = useState(false)
+    const [iconWidth, setIconWidth] = useState('')
+
     return (
         <div>
-            <Button icon={iconMessageLight}
-                    disabled name='Block + primary'
-                    xType='primary'
-                    width='327px'
-                    height='48px'
-                    iconPlace='left'
-                    iconWidth='24px'
-                    iconHeight='24px'
+            <Button isIcon={isIcon}
+                    disabled={disabled}
+                    title='Block + primary'
+                    xType={xType}
+                    buttonWidth={buttonWidth}
+                    buttonHeight={buttonHeight}
+                    iconPosition={iconPlace}
+                    titlePosition={titlePlace}
+                    iconWidth={iconWidth}
+                    iconMarginLeft={iconMarginLeft}
+                    iconMarginRight={iconMarginRight}
             />
+            <div>
+                <b>xType</b>
+                <select value={xType} onChange={(e) => setXType(e.currentTarget.value)}>
+                    {xTypes.map((option) => (
+                        <option
+                            key={option.id}
+                            value={option.value}
+                        >
+                            {option.value}
+                        </option>))}
+                </select>
+            </div>
+            <div>
+                <b>Disabled</b>
+                <input checked={disabled}
+                       type={'checkbox'}
+                       onChange={(e) => setDisabled(e.currentTarget.checked)}/>
+            </div>
+            <div>
+                <b>buttonHeight</b>
+                <select value={buttonHeight} onChange={(e) => setButtonHeight(e.currentTarget.value)}>
+                    {buttonHeights.map((option) => (
+                        <option key={option.id} value={option.value}>
+                            {option.value}
+                        </option>))}
+                </select>
+            </div>
+            <div>
+                <div><b>buttonWidth</b></div>
+                <span>{0}</span>
+                <Slider sx={{width: 200}} size="small" max={400} onChange={(event, value) => {
+                    setButtonWidth(value)
+                }}/>
+                <span>{buttonWidth}</span>
+            </div>
+            <div>
+                <div><b>titlePosition</b></div>
+                <span>{0}</span>
+                <Slider sx={{width: 200}} size="small" onChange={(event, value) => {
+                    setTitlePlace(value)
+                }}/>
+                <span>{titlePlace}</span>
+            </div>
+            <div>
+                <b>isIcon</b>
+                <input checked={isIcon}
+                       type={'checkbox'}
+                       onChange={(e) => setIcon(e.currentTarget.checked)}/>
+            </div>
+            <div>
+                <b>iconWidth</b>
+                <select value={iconWidth} onChange={(e) => setIconWidth(e.currentTarget.value)}>
+                    {iconWidths.map((option) => (
+                        <option key={option.id} value={option.value}>
+                            {option.value}
+                        </option>))}
+                </select>
+            </div>
+            <div>
+                <b>iconPosition</b>
+                <select value={iconPlace} onChange={(e) => setIconPlace(e.currentTarget.value)}>
+                    {iconPlaces.map((option) => (
+                        <option key={option.id} value={option.value}>
+                            {option.value}
+                        </option>))}
+                </select>
+            </div>
+            <div>
+                <div><b>iconMarginLeft</b></div>
+                <span>{0}</span>
+                <Slider sx={{width: 200}} size="small" onChange={(event, value) => {
+                    setIconMarginLeft(value)
+                }}/>
+                <span>{iconMarginLeft}</span>
+            </div>
+            <div>
+                <div><b>iconMarginRight</b></div>
+                <span>{0}</span>
+                <Slider sx={{width: 200}} size="small" onChange={(event, value) => {
+                    setIconMarginRight(value)
+                }}/>
+                <span>{iconMarginRight}</span>
+            </div>
         </div>
     );
 };
--- src/buttons/Button.jsx
+++ src/buttons/Button.jsx
@@ -1,28 +1,42 @@
 import React from 'react';
 import style from './Button.module.css'
 
-const Button = ({className, disabled, name, icon, iconWidth, iconHeight, iconPlace, width, height, xType}) => {
+const Button = ({disabled, title, titlePosition, isIcon, iconMarginLeft, iconMarginRight, iconWidth, iconPosition, buttonWidth, buttonHeight, xType}) => {
 
     const finalClassName = style.button
         + (xType === 'primary'
             ? ' ' + style.primary : xType === 'secondary'
                 ? ' ' + style.secondary : xType === 'outline'
                     ? ' ' + style.outline : xType === 'transparent'
-                        ? style.transparent : '')
-        + (className ? ' ' + className : '');
+                        ? ' ' + style.transparent : '');
 
-    const iconClassName = iconPlace === 'right'
-        ? style.right : iconPlace === 'left'
-            ? style.left : iconPlace === 'side' ? style.side : '';
+    const disabledClassName = style.button
+        + (xType === 'primary' && disabled
+            ? ' ' + style.primaryDisabled : xType === 'secondary' && disabled
+                ? ' ' + style.secondaryDisabled : xType === 'outline' && disabled
+                    ? ' ' + style.outlineDisabled : xType === 'transparent' && disabled
+                        ? ' ' + style.transparentDisabled : '');
+
+    const iconClassName = iconPosition === 'iconRight'
+        ? style.iconRight : iconPosition === 'iconLeft'
+            ? style.iconLeft : iconPosition === 'iconSide' ? style.iconSide : '';
 
     return (
-        <button disabled={disabled} style={{width, height}} className={finalClassName}>
-            {icon && <img className={iconClassName}
-                          style={{width: iconWidth, height: iconHeight}}
-                          src={icon}
-                          alt='icon'/>
+        <button disabled={disabled}
+                style={{width: buttonWidth, height: buttonHeight}}
+                className={finalClassName + ' ' + disabledClassName}>
+            {isIcon && <div style={{width: iconWidth, left: iconMarginLeft, right: iconMarginRight}} className={iconClassName}>
+                <svg style={{width: '100%', height: '100%'}} width="16" height="16" viewBox="0 0 16 16" fill="none"
+                     xmlns="http://www.w3.org/2000/svg">
+                    <path className={finalClassName + ' ' + disabledClassName}
+                          d="M1 8H15M15 8L7.99998 0.999878M15 8L7.99998 14.9999"
+                          stroke="currentColor"
+                          stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+
+            </div>
             }
-            <div className={style.name}>{name}</div>
+                <div style={{marginLeft: titlePosition}} >{title}</div>
         </button>
     );
 };
